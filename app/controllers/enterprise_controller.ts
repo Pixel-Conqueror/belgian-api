c727eaaf66096ec123b5ac96a08d4dad9d2fa26a
ThomasPerez91 <dialldialldiall@gmail.com>
2024-09-26 19:48:29 +0200

    chore: add enterprise request

--- app/controllers/enterprise_controller.ts
+++ app/controllers/enterprise_controller.ts
@@ -5,44 +5,80 @@ export default class EnterpriseController {
   /**
    * @index
    * @description Get paginated enterprises dataset
-   * @params paginate - Pagination options
+   * @params cursor - cursor for pagination
    * @responseBody 200 - <Enterprise>
    * @responseHeader 200
    */
   index = async ({ params }: HttpContext) => {
-    const paginate = params.paginate;
+    const cursor = params.cursor || null;
     const take = 10;
-    const skip = paginate * take;
 
-    const enterprises = await db.enterprise.aggregateRaw({
-      pipeline: [
-        {
-          $lookup: {
-            from: 'denomination',
-            localField: 'enterpriseNumber',
-            foreignField: 'entityNumber',
-            as: 'denominations',
+    const enterprises = await db.enterprise.findMany({
+      take,
+      skip: cursor ? 1 : 0,
+      cursor: cursor ? { enterpriseNumber: cursor } : undefined,
+      include: {
+        denominations: {
+          select: {
+            denomination: true,
+            typeOfDenomination: true,
           },
         },
-        {
-          $lookup: {
-            from: 'activity',
-            localField: 'enterpriseNumber',
-            foreignField: 'entityNumber',
-            as: 'activities',
+        addresses: {
+          select: {
+            streetFR: true,
+            houseNumber: true,
+            zipcode: true,
+            municipalityFR: true,
+            extraAddressInfo: true,
           },
         },
-        {
-          $skip: skip,
-        },
-        {
-          $limit: take,
-        },
-      ],
+      },
+      orderBy: {
+        enterpriseNumber: 'asc',
+      },
     });
 
+    const nextCursor =
+      enterprises.length === take
+        ? enterprises[enterprises.length - 1].enterpriseNumber
+        : null;
+
     return {
-      enterprises,
+      data: enterprises,
+      nextCursor,
+    };
+  };
+
+  /**
+   * @getEnterprise
+   * @description Get enterprise by its number with related data
+   * @params enterpriseNumber - number of the enterprise
+   * @responseBody 200 - <Enterprise>
+   * @responseHeader 200
+   */
+  getEnterprise = async ({ params }: HttpContext) => {
+    const enterpriseNumber = params.id;
+    console.log('enterpriseNumber', enterpriseNumber);
+
+    const enterprise = await db.enterprise.findUnique({
+      where: { enterpriseNumber },
+      include: {
+        activities: {},
+        addresses: {},
+        contacts: {},
+        denominations: {},
+        branches: {},
+        establishments: {},
+      },
+    });
+
+    if (!enterprise) {
+      return { message: 'Enterprise not found', status: 404 };
+    }
+
+    return {
+      data: enterprise,
     };
   };
 }
--- start/routes/enterprise.ts
+++ start/routes/enterprise.ts
@@ -5,6 +5,11 @@ import router from '@adonisjs/core/services/router';
 const EnterpriseController = () => import('#controllers/enterprise_controller');
 
 router
-  .get('/enterprise/:paginate', [EnterpriseController, 'index'])
+  .get('/enterprise/:cursor?', [EnterpriseController, 'index'])
+  .prefix(API_PREFIX_V1)
+  .middleware(middleware.auth());
+
+router
+  .get('/enterprise/find/:id', [EnterpriseController, 'getEnterprise'])
   .prefix(API_PREFIX_V1)
   .middleware(middleware.auth());
